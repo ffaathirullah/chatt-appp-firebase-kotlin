0408357062a37285a0d32e93ff0709a7ff1661ef
Fachrul <ffaathirullah1@gmail.com>
2020-08-22 14:17:28 +0700

    logout pertama

--- app/src/main/java/org/fachrul/faathirullah/chattapp/MainActivity.kt
+++ app/src/main/java/org/fachrul/faathirullah/chattapp/MainActivity.kt
@@ -2,11 +2,28 @@ package org.fachrul.faathirullah.chattapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_logout-> {
+          Toast.makeText(this, "Logout menu Success",Toast.LENGTH_SHORT).show()
+                true
+            }else -> super.onOptionsItemSelected(item)
+        }
     }
 }
